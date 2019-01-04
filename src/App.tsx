c62f2d676f999055e6d1ed9a091ed757f05821cd
Prabeen04 <prabeen.strange@gmail.com>
2019-01-04 21:29:53 +0530

    reactstrap added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>sdlghls</h1>
         <Counter />
       </div>
     );
--- src/Component/Counter/Counter.tsx
+++ src/Component/Counter/Counter.tsx
@@ -5,7 +5,8 @@ const Counter = (props: any) => {
     return (
         <div>
             <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>increase</button>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(0)}>Reset</button>
         </div>
     )
 }
