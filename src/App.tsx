9496c2bd08f19c3b491338a0198dfeddb9657f13
Prabeen04 <prabeen.strange@gmail.com>
2019-03-13 23:53:17 +0530

    Form component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import Counter from './Component/Counter/Counter';
+import Form from './Component/Form/Form';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Counter title='Welcome to Typescript'/>
+        <Counter title='Welcome to Typescript' />
+        <Form />
       </div>
     );
   }
---
+++ src/Component/Form/Form.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default function Form() {
+    return (
+        <div>
+            <h2>Form component</h2>
+        </div>
+    )
+}
+
