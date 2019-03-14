a073be63ea440c8d5187524f00b48897325b0b79
Prabeen04 <prabeen.strange@gmail.com>
2019-03-14 09:08:47 +0530

    form state and change handler created

--- src/Component/Form/Form.tsx
+++ src/Component/Form/Form.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react'
+import TextInput from './TextInput';
 
 export default function Form() {
+    const [task, setTask] = React.useState('')
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setTask(e.target.value)
+    }
     return (
         <div>
             <h2>Form component</h2>
+            <TextInput
+                name='task'
+                placeholder='Enter task'
+                onChange={handleChange}
+                value={task}
+            />
         </div>
     )
 }
