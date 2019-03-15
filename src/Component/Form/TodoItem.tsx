587ed9ae658a172ac45c22cf3af10faf151f4b8b
Prabeen04 <prabeen.strange@gmail.com>
2019-03-15 21:25:18 +0530

    view Edit mode created

--- src/Component/Form/TodoItem.tsx
+++ src/Component/Form/TodoItem.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { ITodo } from "./Form";
+import ViewEdit from "../../Hooks/ViewEdit";
 
 export default function TodoItem(
     { todo, deleteTodo, toggleTodo }
@@ -8,19 +9,26 @@ export default function TodoItem(
             deleteTodo: (todo: ITodo) => void,
             toggleTodo: (todo: ITodo) => void,
         }): JSX.Element {
+    const { viewType, toggleViewType } = ViewEdit()
     return (
         <li className='list-group-item'>
-            <span>
-                <span
-                    onClick={() => toggleTodo(todo)}
-                    style={{ 
-                        fontSize: 24, 
-                        color: todo.isCompleted ? 'red' : 'green',
-                        textDecoration: todo.isCompleted ? 'line-through' : 'none',
-                         }}>
-                    {todo.title}</span>&nbsp;
-                <button className='btn btn-danger' onClick={() => deleteTodo(todo)}>X</button>
-            </span>
+            {
+                viewType === 'view'
+                    ? <span>
+                        <span
+                            onClick={() => toggleTodo(todo)}
+                            style={{
+                                fontSize: 24,
+                                color: todo.isCompleted ? 'red' : 'green',
+                                textDecoration: todo.isCompleted ? 'line-through' : 'none',
+                            }}>
+                            {todo.title}</span>&nbsp;
+                    <button className='btn btn-default' onClick={toggleViewType}>Edit</button>&nbsp;
+                    <button className='btn btn-danger' onClick={() => deleteTodo(todo)}>Delete</button>
+                    </span>
+                    : <p>Edit mode</p>
+            }
+
         </li>
     )
 }
--- src/Hooks/ViewEdit.tsx
+++ src/Hooks/ViewEdit.tsx
@@ -2,12 +2,12 @@ import * as React from 'react'
 
 export default function ViewEdit() {
     const [viewType, setViewType] = React.useState<string>('view')
-    function ToggleViewType(): void {
+    function toggleViewType(): void {
         if (viewType === 'view') {
             setViewType('edit')
         } else {
             setViewType('view')
         }
     }
-    return { viewType, setViewType }
+    return { viewType, toggleViewType }
 }
