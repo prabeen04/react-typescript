0f911cf560ed36d0796bf089d0e57ca29c96af79
Prabeen04 <prabeen.strange@gmail.com>
2019-03-17 18:40:48 +0530

    decreament button added

--- src/Component/Counter/Counter.tsx
+++ src/Component/Counter/Counter.tsx
@@ -11,6 +11,7 @@ const Counter: React.SFC<ICountProps> = (props: ICountProps) => {
             <button onClick={() => console.log('injecting style ....')}>Inject styles</button>
             <h1>{count}</h1>
             <button className='btn btn-primary' onClick={() => setCount(count + 1)}>Increase</button>&nbsp;
+            <button className='btn btn-danger' onClick={() => setCount(count - 1)}>Decrease</button>&nbsp;
             <button className='btn btn-warning' onClick={() => setCount(0)}>Reset</button>
         </div>
     )
