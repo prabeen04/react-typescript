d192d2468a05aaec43d40e9081f0195734774abc
Prabeen04 <prabeen.strange@gmail.com>
2019-03-17 18:59:40 +0530

    useReducer added in counter

--- src/Component/Counter/Counter.tsx
+++ src/Component/Counter/Counter.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { Style } from "../../App";
+const initialState = { count: 0 };
+interface IcounterState {
+    count: string
+}
 interface ICountProps {
 }
 const Counter: React.SFC<ICountProps> = (props: ICountProps) => {
     const style = React.useContext(Style)
     console.log(style)
     const [count, setCount] = React.useState<number>(0);
+    const [state, dispatch] = React.useReducer(appReducer, initialState)
     return (
         <div className='jumbotron'>
             <button onClick={() => console.log('injecting style ....')}>Inject styles</button>
@@ -18,3 +23,17 @@ const Counter: React.SFC<ICountProps> = (props: ICountProps) => {
 }
 
 export default Counter;
+
+const appReducer = (state, action) => {
+    // switch (action.type) {
+    //     case 'INCREASE_TIME':
+    //         return { ...state, time: moment(action.payload).add(1, state.viewType) }
+    //     case 'DECREASE_TIME':
+    //         return { ...state, time: moment(action.payload).subtract(1, state.viewType) }
+    //     case 'CHANGE_VIEW_TYPE':
+    //         return { ...state, viewType: action.payload }
+    //     default:
+    //         return { ...state }
+    // }
+    return state;
+}
