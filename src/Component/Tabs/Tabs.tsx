1c972bf6409699fec8c4a45bf3f806595fb0602e
Prabeen04 <prabeen.strange@gmail.com>
2019-03-18 21:15:04 +0530

    jumbotron added

--- src/Component/Tabs/Tabs.tsx
+++ src/Component/Tabs/Tabs.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Jumbotron, Container, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
 import Counter from '../Counter/Counter'
 import Form from "../Form/Form";
 
-export default function Tabs(props: any) {
+interface ITabsProps {
+
+}
+export default function Tabs(props: ITabsProps) {
     const [activeTab, setActiveTab] = React.useState<string>('1')
     function toggleTab(tab: string) {
         if (activeTab !== tab) {
@@ -18,33 +21,37 @@ export default function Tabs(props: any) {
                     <NavLink
                         className={classnames({ active: activeTab === '1' })}
                         onClick={() => { toggleTab('1'); }}
-                    >
-                        Counter
-            </NavLink>
+                    > Counter </NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink
                         className={classnames({ active: activeTab === '2' })}
                         onClick={() => { toggleTab('2'); }}
-                    >
-                        Todo
-            </NavLink>
+                    > Todo </NavLink>
                 </NavItem>
             </Nav>
             <TabContent activeTab={activeTab}>
                 <TabPane tabId="1">
-                    <Row>
-                        <Col sm="12">
-                            <Counter />
-                        </Col>
-                    </Row>
+                    <Jumbotron fluid>
+                        <Container fluid>
+                            <Row>
+                                <Col sm="12">
+                                    <Counter />
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Jumbotron>
                 </TabPane>
                 <TabPane tabId="2">
-                    <Row>
-                        <Col sm="12">
-                            <Form />
-                        </Col>
-                    </Row>
+                    <Jumbotron fluid>
+                        <Container fluid>
+                            <Row>
+                                <Col sm="12">
+                                    <Form />
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Jumbotron>
                 </TabPane>
             </TabContent>
         </div>
