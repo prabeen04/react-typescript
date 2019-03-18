98e896423b1c23452d081c94827e0adc3f700677
Prabeen04 <prabeen.strange@gmail.com>
2019-03-18 21:00:40 +0530

    Tabs component refactored to functional

--- src/Component/Tabs/Tabs.js
+++ src/Component/Tabs/Tabs.js
@@ -1,63 +1,47 @@
-import React from 'react';
+import * as React from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
 import Counter from '../Counter/Counter'
 import Form from "../Form/Form";
-export default class Example extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            activeTab: '1'
-        };
-    }
-
-    toggle(tab) {
-        if (this.state.activeTab !== tab) {
-            this.setState({
-                activeTab: tab
-            });
-        }
-    }
-    render() {
-        return (
-            <div>
-                <Nav tabs>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: this.state.activeTab === '1' })}
-                            onClick={() => { this.toggle('1'); }}
-                        >
-                            Counter
+export default function Tabs(props) {
+    const [activeTab, setActiveTab] = React.useState('1')
+    return (
+        <div>
+            <Nav tabs>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '1' })}
+                        onClick={() => { this.toggle('1'); }}
+                    >
+                        Counter
             </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: this.state.activeTab === '2' })}
-                            onClick={() => { this.toggle('2'); }}
-                        >
-                            Todo
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '2' })}
+                        onClick={() => { this.toggle('2'); }}
+                    >
+                        Todo
             </NavLink>
-                    </NavItem>
-                </Nav>
-                <TabContent activeTab={this.state.activeTab}>
-                    <TabPane tabId="1">
-                        <Row>
-                            <Col sm="12">
-                                <Counter />
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="2">
-                        <Row>
-                            <Col sm="12">
-                                <Form />
-                            </Col>
-                        </Row>
-                    </TabPane>
-                </TabContent>
-            </div>
-        );
-    }
+                </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">
+                    <Row>
+                        <Col sm="12">
+                            <Counter />
+                        </Col>
+                    </Row>
+                </TabPane>
+                <TabPane tabId="2">
+                    <Row>
+                        <Col sm="12">
+                            <Form />
+                        </Col>
+                    </Row>
+                </TabPane>
+            </TabContent>
+        </div>
+    );
 }
