3fe64118a46e6770bfdeb360fe6973190ea7440a
Prabeen04 <prabeen.strange@gmail.com>
2019-03-19 20:14:36 +0530

    user type interface and state initialized

--- src/Hooks/UserData.js
+++ src/Hooks/UserData.js
@@ -1,6 +1,20 @@
 import * as React from 'react'
+
 const url = 'https://jsonplaceholder.typicode.com/users';
 
+export interface IUser {
+    fetchingUsers: boolean;
+    users: any;
+    fetchingUsersError: boolean
+}
+const initialState = {
+    fetchingUsers: false,
+    users: [],
+    fetchingUsersError: false
+}
+
 export default function UserData() {
+    const [usersState, setUsersState] = React.useState<IUser>(initialState)
+
     return null
 }
