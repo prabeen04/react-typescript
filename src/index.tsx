9b94a20b74efc90d9979a5695bf1c50dcbd89e09
Prabeen04 <prabeen.strange@gmail.com>
2019-03-19 19:48:17 +0530

    small stuff

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 
