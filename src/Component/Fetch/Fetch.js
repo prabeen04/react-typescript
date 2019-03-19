2e5586802b522d957bc7b1b68a3313fe4b758ba5
Prabeen04 <prabeen.strange@gmail.com>
2019-03-19 20:51:23 +0530

    users displayed

--- src/Component/Fetch/Fetch.js
+++ src/Component/Fetch/Fetch.js
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import useUserData, { IUser } from "../../Hooks/UserData";
+
 export default function Fetch() {
-    const { fetchingUsers, fetchingUsersError, users: IUser } = useUserData();
+    const { fetchingUsers, fetchingUsersError, users} = useUserData();
+    if (fetchingUsers) {
+        return <p>fetching users...</p>
+    }
+    if (fetchingUsersError) {
+        return <p>Opps, Error while fetching users...</p>
+    }
     return (
         <>
-
+            {users && users.map(user => <p>{user.name}</p>)}
         </>
     )
 }
