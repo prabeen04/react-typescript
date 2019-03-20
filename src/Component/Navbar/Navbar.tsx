ba844f3398358bd40cd540ac55c4ea9f49b377fe
Prabeen04 <prabeen.strange@gmail.com>
2019-03-20 20:20:37 +0530

    Navbar refactored to hooks

--- src/Component/Navbar/Navbar.tsx
+++ src/Component/Navbar/Navbar.tsx
@@ -2,54 +2,36 @@ import * as React from 'react';
 import { Link } from "react-router-dom";
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, } from 'reactstrap';
 
-interface INavbarState {
-    isOpen: boolean
-}
-interface INavbarProps {
-
-}
-
-export default class MyNavbar extends React.Component<INavbarProps, INavbarState> {
-    constructor(props: INavbarProps) {
-        super(props);
-
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
+export default function MyNavbar() {
+    const [isOpen, setIsOpen] = React.useState<boolean>(false);
+    function toggle() {
+        setIsOpen(!isOpen)
     }
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-    render() {
-        return (
-            <div>
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/"><Link to="/">Typescript</Link></NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink>
-                                    <Link to="/counter">Counter</Link>
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink>
-                                    <Link to="/todo">Todo</Link>
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink>
-                                    <Link to="/fetch">Users</Link>
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/"><Link to="/">Typescript</Link></NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink>
+                                <Link to="/counter">Counter</Link>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink>
+                                <Link to="/todo">Todo</Link>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink>
+                                <Link to="/fetch">Users</Link>
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
 }
