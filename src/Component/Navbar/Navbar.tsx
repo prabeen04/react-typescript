5c241b34de04fb6731a0ab65883006ca07a4a9e7
Prabeen04 <prabeen.strange@gmail.com>
2019-03-20 20:09:38 +0530

    code cleaned

--- src/Component/Navbar/Navbar.tsx
+++ src/Component/Navbar/Navbar.tsx
@@ -7,10 +7,6 @@ import {
     Nav,
     NavItem,
     NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem
 } from 'reactstrap';
 import { Link } from "react-router-dom";
 export default class MyNavbar extends React.Component<any, any> {
@@ -45,23 +41,6 @@ export default class MyNavbar extends React.Component<any, any> {
                                     <Link to="/todo">Todo</Link>
                                 </NavLink>
                             </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Options
-                </DropdownToggle>
-                                <DropdownMenu right>
-                                    <DropdownItem>
-                                        Option 1
-                  </DropdownItem>
-                                    <DropdownItem>
-                                        Option 2
-                  </DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem>
-                                        Reset
-                  </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
                         </Nav>
                     </Collapse>
                 </Navbar>
