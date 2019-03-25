e4462fb6d57ba14b0a85465feb41782db4cdd1b5
Prabeen04 <prabeen.strange@gmail.com>
2019-03-25 21:23:06 +0530

    user Set

--- src/Component/Fetch/Fetch.tsx
+++ src/Component/Fetch/Fetch.tsx
@@ -5,12 +5,16 @@ import './Fetch.css';
 
 export default function Fetch() {
     const { fetchingUsers, fetchingUsersError, users } = useUserData();
-    const [activeUser, setActiveUser] = React.useState({})
+    const [activeUser, setActiveUser] = React.useState<IUser>(users.length && users[0] || {})
     React.useEffect(() => {
-        if(users.length){
+        if (users.length) {
             setActiveUser(users[0])
         }
-    })
+    }, [users])
+    function setUser(user: IUser) {
+        console.log(user)
+        setActiveUser(user)
+    }
     return (
         <>
             <div className='user-wrapper flex-container space-between'>
@@ -21,10 +25,11 @@ export default function Fetch() {
                             ? <p>Opps, Error while fetching users...</p>
                             : <>
                                 <div className='user-list'>
-                                    {users && users.map((user: IUser) => <UserList activeUser={activeUser} key={user.id} {...user} />)}
+                                    {users && users.map((user: IUser) => <UserList setUser={setUser} activeUser={activeUser} key={user.id} {...user} />)}
                                 </div>
-                                <div className='user-detail'>
-                                </div>
+                                {activeUser.hasOwnProperty('name') && <div className='user-detail'>
+                                    <p>{activeUser.name}</p>
+                                </div>}
                             </>
                 }
 
--- src/Component/Fetch/UserList.tsx
+++ src/Component/Fetch/UserList.tsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import { IUser } from "../../Hooks/UserData";
 
 export default function UserList(props: any) {
-    const { activeUser, ...user } = props
+    const { activeUser, setUser, ...user } = props
     console.log(props)
     return (
-        <div className={`user-list-item ${user.id === activeUser.id ? 'active-list' : ''}`}>
+        <div
+            className={`user-list-item ${user.id === activeUser.id ? 'active-list' : ''}`}
+            onClick={() => setUser(user)}
+        >
             {props.name}
         </div>
     )
