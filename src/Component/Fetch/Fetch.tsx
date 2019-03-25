c761b6f828704fecb14cecf83aba87f884cb276d
Prabeen04 <prabeen.strange@gmail.com>
2019-03-25 20:46:09 +0530

    small stuff

--- src/Component/Fetch/Fetch.tsx
+++ src/Component/Fetch/Fetch.tsx
@@ -5,23 +5,23 @@ import './Fetch.css';
 
 export default function Fetch() {
     const { fetchingUsers, fetchingUsersError, users } = useUserData();
-    console.log(users)
-    if (fetchingUsers) {
-        return <p>fetching users...</p>
-    }
-    if (fetchingUsersError) {
-        return <p>Opps, Error while fetching users...</p>
-    }
     return (
         <>
             <div className='user-wrapper flex-container space-between'>
-                <div className='user-list'>
-                    {users && users.map((user: IUser) => <UserList key={user.id} {...user} />)}
-                </div>
-                <div className='user-detail'>
+                {
+                    fetchingUsers
+                        ? <p>fetching users...</p>
+                        : fetchingUsersError
+                            ? <p>Opps, Error while fetching users...</p>
+                            : <>
+                                <div className='user-list'>
+                                    {users && users.map((user: IUser) => <UserList key={user.id} {...user} />)}
+                                </div>
+                                <div className='user-detail'>
+                                </div>
+                            </>
+                }
 
-
-                </div>
 
             </div>
         </>
