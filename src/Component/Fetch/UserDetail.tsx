87949b40abe8fc43cc74cbd8dcf65b52b59673c0
Prabeen04 <prabeen.strange@gmail.com>
2019-03-26 09:23:37 +0530

    more styles addded

--- src/Component/Fetch/UserDetail.tsx
+++ src/Component/Fetch/UserDetail.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import { IUser, IAddress, IGeo, ICompany } from "../../Hooks/UserData";
-export default function UserDetail(props: IUser){
-    return (
-        <div className='user-details'>
 
+export interface IUserDetail {
+    user: IUser;
+}
+export default function UserDetail(props: IUserDetail) {
+    const { user } = props;
+    return (
+        <div className='user-detail'>
+            <div className="user-detail-header">
+                <h1>{user.name}</h1>
+            </div>
 
         </div>
     )
