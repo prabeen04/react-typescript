9de88c1df7181213409702f662d7ada368959f34
Prabeen04 <prabeen.strange@gmail.com>
2019-03-26 09:33:31 +0530

    UserDetail stuff

--- src/Component/Fetch/UserDetail.tsx
+++ src/Component/Fetch/UserDetail.tsx
@@ -12,6 +12,11 @@ export default function UserDetail(props: IUserDetail) {
                 <h1>{user.name}</h1>
             </div>
 
+            <div className="user-detail-body">
+                <p>{user.email}</p>
+                <p>{user.phone}</p>
+                <a href={user.website} target='_blank'>{user.website}</a>
+            </div>
         </div>
     )
 }
