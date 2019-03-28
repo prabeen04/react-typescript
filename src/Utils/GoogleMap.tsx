f07a8a1aeb8feee1ec6d9153476b5e1c79a6104d
Prabeen04 <prabeen.strange@gmail.com>
2019-03-28 10:06:34 +0530

    google-maps-react types

---
+++ src/Utils/GoogleMap.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+class GoogleMap extends Component {
+    render() {
+        return (
+            <div style={{ marginTop: 20}}>
+                <Map
+                    google={this.props.google}
+                    style={{ width: 700, height: 250 }}
+                    zoom={13}
+                    initialCenter={{
+                        lat: 40.854885,
+                        lng: -88.081807
+                    }}
+                    center={this.props.latLng}
+                >
+
+                    <Marker onClick={this.onMarkerClick}
+                        name={'Current location'}
+                        title={'Default value'}
+                        position={this.props.latLng} />
+
+                    <Marker onClick={this.onMarkerClick}
+                        name={'Current location'}
+                        title={'Searched value'}
+                        position={{
+                            lat: 40.854885,
+                            lng: -88.081807
+                        }} />
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                        <div>
+                            <h1>{'this.state.selectedPlace.name'}</h1>
+                        </div>
+                    </InfoWindow>
+                </Map>
+            </div>
+        )
+    }
+}
+
+// export default GoogleMap;
+export default GoogleApiWrapper({
+    apiKey: ''
+})(GoogleMap)
