9b960982bc6f851a234e309db33a44aab5a507f8
Prabeen04 <prabeen.strange@gmail.com>
2019-03-28 10:07:41 +0530

    small sutff

--- src/Utils/GoogleMap.tsx
+++ src/Utils/GoogleMap.tsx
@@ -1,45 +1,45 @@
-import React, { Component } from 'react'
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+// import * as React from 'react'
+// import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+// import { GOOGLE_API_KEY } from '../Config/Keys';
+// class GoogleMap extends React.Component {
+//     render() {
+//         return (
+//             <div style={{ marginTop: 20}}>
+//                 <Map
+//                     google={this.props.google}
+//                     style={{ width: 700, height: 250 }}
+//                     zoom={13}
+//                     initialCenter={{
+//                         lat: 40.854885,
+//                         lng: -88.081807
+//                     }}
+//                     center={this.props.latLng}
+//                 >
 
-class GoogleMap extends Component {
-    render() {
-        return (
-            <div style={{ marginTop: 20}}>
-                <Map
-                    google={this.props.google}
-                    style={{ width: 700, height: 250 }}
-                    zoom={13}
-                    initialCenter={{
-                        lat: 40.854885,
-                        lng: -88.081807
-                    }}
-                    center={this.props.latLng}
-                >
+//                     <Marker onClick={this.onMarkerClick}
+//                         name={'Current location'}
+//                         title={'Default value'}
+//                         position={this.props.latLng} />
 
-                    <Marker onClick={this.onMarkerClick}
-                        name={'Current location'}
-                        title={'Default value'}
-                        position={this.props.latLng} />
+//                     <Marker onClick={this.onMarkerClick}
+//                         name={'Current location'}
+//                         title={'Searched value'}
+//                         position={{
+//                             lat: 40.854885,
+//                             lng: -88.081807
+//                         }} />
+//                     <InfoWindow onClose={this.onInfoWindowClose}>
+//                         <div>
+//                             <h1>{'this.state.selectedPlace.name'}</h1>
+//                         </div>
+//                     </InfoWindow>
+//                 </Map>
+//             </div>
+//         )
+//     }
+// }
 
-                    <Marker onClick={this.onMarkerClick}
-                        name={'Current location'}
-                        title={'Searched value'}
-                        position={{
-                            lat: 40.854885,
-                            lng: -88.081807
-                        }} />
-                    <InfoWindow onClose={this.onInfoWindowClose}>
-                        <div>
-                            <h1>{'this.state.selectedPlace.name'}</h1>
-                        </div>
-                    </InfoWindow>
-                </Map>
-            </div>
-        )
-    }
-}
-
-// export default GoogleMap;
-export default GoogleApiWrapper({
-    apiKey: ''
-})(GoogleMap)
+// // export default GoogleMap;
+// export default GoogleApiWrapper({
+//     apiKey: ''
+// })(GoogleMap)
