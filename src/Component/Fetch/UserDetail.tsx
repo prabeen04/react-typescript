f0f04cace5ad817c4d8d852694fc045941381889
Prabeen04 <prabeen.strange@gmail.com>
2019-03-28 08:40:06 +0530

    Modal rendered in userDetails

--- src/Component/Fetch/UserDetail.tsx
+++ src/Component/Fetch/UserDetail.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useSpring, animated } from 'react-spring';
 import { IUser, IAddress, IGeo, ICompany } from "../../Hooks/UserData";
-
+import UserAddressModal from "../../Utils/UserAddressModal";
 export interface IUserDetail {
     user: IUser;
 }
@@ -9,24 +9,33 @@ export default function UserDetail(props: IUserDetail) {
     const styles = useSpring({ opacity: 1, transform: 'translate3d(0px,0,0) ', from: { opacity: 0, transform: 'translate3d(0,200px,0) ', } })
     const { user } = props;
     return (
-        <div className='user-detail'>
-            <animated.div style={styles}>
-                <div className="user-detail-header">
-                    <h1><i className="fas fa-map-marker-alt">&nbsp;&nbsp;</i>{user.name} </h1>
-                </div>
+        <>
+            <div className='user-detail'>
+                <animated.div style={styles}>
+                    <div className="user-detail-header">
+                        <h1><i className="fas fa-map-marker-alt clickable-icon">&nbsp;&nbsp;</i>{user.name} </h1>
+                    </div>
 
-                <div className="user-detail-body">
-                    <p>{user.email}</p>
-                    <p>{user.phone}</p>
-                    <a href={user.website} target='_blank'>{user.website}</a>
-                </div>
-                <div className="user-detail-body">
-                    <p>{user.address.suite}</p>
-                    <p>{user.address.street}</p>
-                    <p>{user.address.city}</p>
-                    <p>{user.address.zipcode}</p>
-                </div>
-            </animated.div>
-        </div>
+                    <div className="user-detail-body">
+                        <p>{user.email}</p>
+                        <p>{user.phone}</p>
+                        <a href={user.website} target='_blank'>{user.website}</a>
+                    </div>
+                    <div className="user-detail-body">
+                        <p>{user.address.suite}</p>
+                        <p>{user.address.street}</p>
+                        <p>{user.address.city}</p>
+                        <p>{user.address.zipcode}</p>
+                    </div>
+                </animated.div>
+            </div>
+            <UserAddressModal
+                title='Address'
+                visible={true}
+                toggle={() => { }}
+            >
+                <p>children</p>
+            </UserAddressModal>
+        </>
     )
 }
