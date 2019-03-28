8e1e772172b2de09235f22414b025f37666842cf
Prabeen04 <prabeen.strange@gmail.com>
2019-03-28 08:34:59 +0530

    spring animation moved to inside userDetails

--- src/Component/Fetch/Fetch.tsx
+++ src/Component/Fetch/Fetch.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useSpring, animated } from 'react-spring';
 import useUserData, { IUserState, IUser, IAddress, ICompany, IGeo } from "../../Hooks/UserData";
 import UserList from "./UserList";
 import UserDetail from './UserDetail';
@@ -8,7 +7,6 @@ import './Fetch.css';
 export default function Fetch() {
     const { fetchingUsers, fetchingUsersError, users } = useUserData();
     const [activeUser, setActiveUser] = React.useState<IUser>(users.length && users[0] || {})
-    const styles = useSpring({ opacity: 1, transform: 'translate3d(0px,0,0) ', from: { opacity: 0, transform: 'translate3d(0,200px,0) ', } })
     React.useEffect(() => {
         if (users.length) {
             setActiveUser(users[0])
@@ -33,10 +31,7 @@ export default function Fetch() {
                                     {users && users.map((user: IUser) => <UserList setUser={setUser} activeUser={activeUser} key={user.id} user={user} />)}
                                 </div>
                                 {activeUser.hasOwnProperty('name')
-                                    &&
-                                    <animated.div style={styles}>
-                                        <UserDetail user={activeUser} />
-                                    </animated.div>
+                                    && <UserDetail user={activeUser} />
                                 }
                             </>
                 }
--- src/Component/Fetch/UserDetail.tsx
+++ src/Component/Fetch/UserDetail.tsx
@@ -1,28 +1,32 @@
 import * as React from 'react';
+import { useSpring, animated } from 'react-spring';
 import { IUser, IAddress, IGeo, ICompany } from "../../Hooks/UserData";
 
 export interface IUserDetail {
     user: IUser;
 }
 export default function UserDetail(props: IUserDetail) {
+    const styles = useSpring({ opacity: 1, transform: 'translate3d(0px,0,0) ', from: { opacity: 0, transform: 'translate3d(0,200px,0) ', } })
     const { user } = props;
     return (
         <div className='user-detail'>
-            <div className="user-detail-header">
-                <h1><i className="fas fa-map-marker-alt">&nbsp;&nbsp;</i>{user.name} </h1>
-            </div>
+            <animated.div style={styles}>
+                <div className="user-detail-header">
+                    <h1><i className="fas fa-map-marker-alt">&nbsp;&nbsp;</i>{user.name} </h1>
+                </div>
 
-            <div className="user-detail-body">
-                <p>{user.email}</p>
-                <p>{user.phone}</p>
-                <a href={user.website} target='_blank'>{user.website}</a>
-            </div>
-            <div className="user-detail-body">
-                <p>{user.address.suite}</p>
-                <p>{user.address.street}</p>
-                <p>{user.address.city}</p>
-                <p>{user.address.zipcode}</p>
-            </div>
+                <div className="user-detail-body">
+                    <p>{user.email}</p>
+                    <p>{user.phone}</p>
+                    <a href={user.website} target='_blank'>{user.website}</a>
+                </div>
+                <div className="user-detail-body">
+                    <p>{user.address.suite}</p>
+                    <p>{user.address.street}</p>
+                    <p>{user.address.city}</p>
+                    <p>{user.address.zipcode}</p>
+                </div>
+            </animated.div>
         </div>
     )
 }
