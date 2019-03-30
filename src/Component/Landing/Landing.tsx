7503a5aae2d088e7d3ce0800496913c602b76660
Prabeen04 <prabeen.strange@gmail.com>
2019-03-30 15:27:44 +0530

    small stuff

--- src/Component/Landing/Landing.tsx
+++ src/Component/Landing/Landing.tsx
@@ -4,12 +4,10 @@ import './Landing.css';
 
 export default function Landing() {
     const points: string[] = [' POWER ', ' STYLE ', ' AWESOMENESS ', ' EVERYTHING ']
-    const styles = useSpring({ opacity: 1, transform: 'translate3d(0px,0,0) ', from: { opacity: 0, transform: 'translate3d(0,200px,0) ', } })
-    const textStyles = useSpring(
-        {
-            transform: 'translate3d(0,-400px,0)', color: 'red',
-            from: { transform: 'translate3d(0,0px,0)', color: 'blue' }
-        })
+    const styles = useSpring({
+        opacity: 1, transform: 'translate3d(0px,0,0) ', rotate: '0',
+        from: { opacity: 0, transform: 'translate3d(0,200px,0) ', rotate: '180', }
+    })
     const [title, setTitle] = React.useState<string>(points[0])
 
     React.useEffect(() => {
@@ -35,15 +33,40 @@ export default function Landing() {
         <div className='landing-wrapper'>
             <div className="welcome-text">
                 <animated.div style={styles}>
-                    <h1>Let's explore the
-                        {
-                            <animated.span style={textStyles}>
-                                {title}
-                            </animated.span>
-                        }
-                        of typescript</h1>
+                    <>
+                        <h1>Let's explore the
+                        {<Title title={title} />}
+                            of typescript</h1>
+                    </>
                 </animated.div>
             </div>
         </div>
+    )
+}
+
+function Title({ title }: any) {
+    const textStyles = useSpring(
+        {
+            transform: 'translate3d(0,-400px,0)',
+            color: 'red',
+            opacity: 1,
+            from: {
+                transform: 'translate3d(0,0px,0)',
+                color: 'blue',
+                opacity: 0,
+            },
+            // enter: { opacity: 1, transform: 'translateY(0)' },
+            // leave: { opacity: 0, transform: 'translateY(-100px)', pointerEvents: 'none' },
+            // // update: {
+            // //     transform: 'translate3d(0,0px,0)',
+            // //     color: 'blue',
+            // //     opacity: 0,
+            // // },
+            delay: 1000
+        })
+    return (
+        <animated.span style={textStyles}>
+            {title}
+        </animated.span>
     )
 }
