b387b5dccefded85bc6a71a0e5dc0a913f49eb19
Prabeen04 <prabeen.strange@gmail.com>
2019-03-31 09:28:49 +0530

    Landing snapshot updated

--- src/Component/Landing/__test__/Landing.test.tsx
+++ src/Component/Landing/__test__/Landing.test.tsx
@@ -3,11 +3,15 @@ import { shallow } from 'enzyme';
 import Landing from '../Landing';
 
 describe('<Landing />', () => {
-    const wrapper = shallow(<Landing />);
-    const landingWrapper = wrapper.find('[data-test="Landing"]');
 
     it('should match snapshot', () => {
+        const wrapper = shallow(<Landing />);
         expect(wrapper).toMatchSnapshot()
     })
 
+    it('should render the Landing wrapper', () => {
+        const wrapper = shallow(<Landing />);
+        const landingWrapper = wrapper.find('[data-test="Landing"]');
+        expect(landingWrapper.length).toEqual(1)
+    })
 })
