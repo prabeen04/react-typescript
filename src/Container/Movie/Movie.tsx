02cf017690ae5e1267004142b8a84bd6736172ab
Prabeen04 <prabeen.strange@gmail.com>
2019-04-02 20:56:51 +0530

    movies data displayed on movie page

--- src/Container/Movie/Movie.tsx
+++ src/Container/Movie/Movie.tsx
@@ -3,10 +3,21 @@ import useMovie from './UseMovie';
 
 export default function Movie() {
     const { state, dispatch } = useMovie();
+    const { fetchingMovies, fetchingMoviesError, movies } = state;
     console.log(state)
+    if (fetchingMovies) {
+        return (
+            <div className="flex-container center-align-row" style={{ width: '100%', padding: 100 }}>
+                <div className="loading"></div>
+            </div>
+        )
+    }
+    if (fetchingMoviesError) {
+        return <h3>Error fetvhing movies</h3>
+    }
     return (
-        <div style={{height: 200}}>
-            <h3>Movie component</h3>
+        <div style={{ height: 200 }}>
+            {movies.map((movie: any) => <p>{movie.original_title}</p>)}
         </div>
     )
 }
