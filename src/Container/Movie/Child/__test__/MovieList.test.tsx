4e5773642c099c6e03cc87193191d172be4a4acb
Prabeen04 <prabeen.strange@gmail.com>
2019-04-03 23:48:17 +0530

    MovieList render test

--- src/Container/Movie/Child/__test__/MovieList.test.tsx
+++ src/Container/Movie/Child/__test__/MovieList.test.tsx
@@ -8,4 +8,9 @@ describe('<MovieList/>', () => {
         const wrapper = shallow(<MovieList movies={[]} />)
         expect(wrapper).toMatchSnapshot()
     })
+    it('should render Movielist', () => {
+        const wrapper = shallow(<MovieList movies={[]} />)
+        const movieList = wrapper.find('[data-test="MovieList"]')
+        expect(movieList.length).toEqual(1)
+    })
 })
