f3e6c1f4808b4085934b55c89ffb8e1bcceda283
Prabeen04 <prabeen.strange@gmail.com>
2019-04-03 23:54:07 +0530

    landing snapshot removed

--- src/Component/Landing/__test__/Landing.test.tsx
+++ src/Component/Landing/__test__/Landing.test.tsx
@@ -3,10 +3,6 @@ import { shallow } from 'enzyme';
 import Landing from '../Landing';
 
 describe('<Landing />', () => {
-    it('should match snapshot', () => {
-        const wrapper = shallow(<Landing />);
-        expect(wrapper).toMatchSnapshot()
-    })
     it('should render the Landing wrapper', () => {
         const wrapper = shallow(<Landing />);
         const landingWrapper = wrapper.find('[data-test="Landing"]');
