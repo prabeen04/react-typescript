61c2b829d467c3bb36f43686d0a15806cf0fdbc1
Prabeen04 <prabeen.strange@gmail.com>
2019-04-07 21:06:23 +0530

    data fetched from graphql server

--- src/Container/Graphql/SampleQuery.tsx
+++ src/Container/Graphql/SampleQuery.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
@@ -12,12 +13,13 @@ const SampleQuery = () => (
     `}
     >
         {({ loading, error, data }) => {
+            console.log(data)
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error :(</p>;
 
-            return data.rates.map(({ currency, rate }: { currency: any, rate: any }) => (
-                <div key={currency}>
-                    <p>{currency}: {rate}</p>
+            return data && data.users.map(({ userName }: { userName: any }) => (
+                <div key={userName}>
+                    <p>Username: {userName}</p>
                 </div>
             ));
         }}
