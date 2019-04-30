b5f8f98ac4aa1dee8edae8684c66827461a34d54
Prabeen04 <prabeen.strange@gmail.com>
2019-04-30 20:33:00 +0530

    Stoty interface moved to new file

--- src/Container/Graphql/Story/Story.tsx
+++ src/Container/Graphql/Story/Story.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
+import { IStory } from './StoryInterface';
 
-export interface IStory {
-    title: String
-}
 export default function Story(props: IStory) {
     return <h1 className='story-title' data-test="Story">{props.title}</h1>
 }
---
+++ src/Container/Graphql/Story/StoryInterface.ts
@@ -0,0 +1,4 @@
+
+export interface IStory {
+    title: String
+}
