5581f9df441937c1e9faadb49febac6abab477de
Prabeen04 <prabeen.strange@gmail.com>
2019-05-02 20:06:10 +0530

    handleSubmit created

--- src/Container/Graphql/Story/Child/AddStoryModal.tsx
+++ src/Container/Graphql/Story/Child/AddStoryModal.tsx
@@ -9,7 +9,10 @@ export default function AddStoryModal() {
     const { state, dispatch } = React.useContext(StoryContext)
     const [storyTitle, setStoryTitle] = React.useState<string>('')
     const [user, setUser] = React.useState<string>('')
-
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault()
+        console.log(storyTitle, user)
+    }
     return (
         <Modal
             noHeader
@@ -17,24 +20,26 @@ export default function AddStoryModal() {
             toggle={() => dispatch({ type: types.TOGGLE_ADD_STORY_MODAL, payload: false })}
         >
             <>
-                <TextInput
-                    className='form-control'
-                    name='title'
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setStoryTitle(e.target.value)}
-                    value={storyTitle}
-                    placeholder="What's the story ?"
-                /><br />
-                <SelectInput
-                    className='form-control'
-                    name='title'
-                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setUser(e.target.value)}
-                    value={user}
-                    placeholder="What's the story ?"
-                    options={[{ label: 'one', value: 1 }, { label: 'two', value: 2 }, { label: 'three', value: 3 },]}
-                /><br />
-                <button
-                    className='btn btn-primary'
-                    onClick={() => { }}>Add</button>
+                <form onSubmit={handleSubmit}>
+                    <TextInput
+                        className='form-control'
+                        name='title'
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setStoryTitle(e.target.value)}
+                        value={storyTitle}
+                        placeholder="What's the story ?"
+                    /><br />
+                    <SelectInput
+                        className='form-control'
+                        name='title'
+                        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setUser(e.target.value)}
+                        value={user}
+                        placeholder="What's the story ?"
+                        options={[{ label: 'one', value: 1 }, { label: 'two', value: 2 }, { label: 'three', value: 3 },]}
+                    /><br />
+                    <button
+                        className='btn btn-primary'
+                    >Add</button>
+                </form>
             </>
         </Modal>
     )
