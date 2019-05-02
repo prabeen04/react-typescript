db44f99f5ae226bffd83a55574a8c1deed4fa341
Prabeen04 <prabeen.strange@gmail.com>
2019-05-02 09:06:37 +0530

    select input component created

---
+++ src/Component/Form/SelectInput.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+export interface ISelectInputInputProps {
+    value: string;
+    name: string;
+    onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+    placeholder?: string;
+    className?: string;
+    options: any[];
+    style?: object
+}
+export default function SelectInputInput(props: ISelectInputInputProps) {
+    const { name, value, placeholder, onChange, options, ...rest } = props;
+    return (
+        <>
+            <select
+                data-test='SelectInputInput'
+                name={name}
+                value={value}
+                placeholder={placeholder}
+                onChange={onChange}
+                {...rest}
+            >
+            {options.map((option: any, i: number) => <option key={i} value={option.value}>{option.label}</option>)}
+            </select>
+        </>
+    )
+}
