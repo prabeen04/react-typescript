4b973d03069270a75c74150e4475101ff88aa204
Prabeen04 <prabeen.strange@gmail.com>
2019-05-05 22:01:42 +0530

    small stuff

--- src/Container/Graphql/Story/Child/StoryDetail.tsx
+++ src/Container/Graphql/Story/Child/StoryDetail.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 
 export default function StoryDetail() {
   return (
-    <>
+    <div className='story-detail' data-test='StoryDetail'>
       <p>Story details</p>
-    </>
+    </div>
   )
 }
