4721b1de2a429570eae6132e36ed60afa5d5b7d7
Prabeen04 <prabeen.strange@gmail.com>
2019-05-05 22:10:08 +0530

    props passed to singleStory

--- src/Container/Graphql/Story/Child/__test__/SingleStory.test.tsx
+++ src/Container/Graphql/Story/Child/__test__/SingleStory.test.tsx
@@ -3,8 +3,11 @@ import { shallow } from "enzyme";
 import SingleStory from '../SingleStory';
 
 describe('<SingleStory/>', () => {
+    const props = {
+        title: 'test'
+    }
     it('should match snapshot', () => {
-        const wrapper = shallow(<SingleStory />)
+        const wrapper = shallow(<SingleStory {...props} />)
         expect(wrapper).toMatchSnapshot()
     })
 })
