31a4da10bbb212e4764e2997a690bf3b66ecd392
Prabeen04 <prabeen.strange@gmail.com>
2019-05-10 19:55:45 +0530

    small stuff

--- src/Container/Graphql/Story/Child/StoryDetail.tsx
+++ src/Container/Graphql/Story/Child/StoryDetail.tsx
@@ -3,11 +3,9 @@ import useStory from "../useStory";
 
 export default function StoryDetail() {
   const { state, dispatch } = useStory()
-  console.log(state)
   return (
     <div className='story-detail' data-test='StoryDetail'>
       <p>Story details</p>
-      <h2>{state.selectedStory}</h2>
     </div>
   )
 }
