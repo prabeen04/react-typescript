3e7c41a81b5a0be221a79e75babb523fca11dc52
Prabeen04 <prabeen.strange@gmail.com>
2019-05-10 19:27:30 +0530

    setSelected story

--- src/Container/Graphql/Story/Child/StoryDetail.tsx
+++ src/Container/Graphql/Story/Child/StoryDetail.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
+import useStory from "../useStory";
 
 export default function StoryDetail() {
+  const { state, dispatch } = useStory()
   return (
     <div className='story-detail' data-test='StoryDetail'>
       <p>Story details</p>
+      <h2>{state.selectedStory}</h2>
     </div>
   )
 }
--- src/Container/Graphql/Story/useStory.tsx
+++ src/Container/Graphql/Story/useStory.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import * as types from './StoryActionTypes';
 
 export const initialState: any = {
-    addStoryModal: false
+    addStoryModal: false,
+    selectedStory: null,
 }
 export default function useStory() {
     const [state, dispatch] = React.useReducer(storyReducer, initialState)
@@ -13,6 +14,8 @@ function storyReducer(state: any, action: any) {
     switch (action.type) {
         case types.TOGGLE_ADD_STORY_MODAL:
             return { ...state, addStoryModal: action.payload }
+        case types.SET_SELECTED_STORY:
+            return { ...state, selectedStory: action.payload }
         default:
             throw new Error('should match a case');
     }
