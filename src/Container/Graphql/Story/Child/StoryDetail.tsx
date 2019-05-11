566d3830573bbae62211634d20070f350fc9550f
Prabeen04 <prabeen.strange@gmail.com>
2019-05-11 16:17:41 +0530

    bug fixes

--- src/Container/Graphql/Story/Child/StoryDetail.tsx
+++ src/Container/Graphql/Story/Child/StoryDetail.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
 import useStory from "../useStory";
+import StoryContext from "../StoryContext";
 
 export default function StoryDetail() {
   const { state, dispatch } = useStory()
+  const test = React.useContext(StoryContext)
+  console.log(test)
+  console.log(useStory())
   return (
     <div className='story-detail' data-test='StoryDetail'>
       <p>Story details</p>
--- src/Container/Graphql/Story/useStory.tsx
+++ src/Container/Graphql/Story/useStory.tsx
@@ -10,12 +10,12 @@ export default function useStory() {
     return { state, dispatch }
 }
 
-function storyReducer(state: any, action: any) {
+function storyReducer(state: any = initialState, action: any) {
     switch (action.type) {
         case types.TOGGLE_ADD_STORY_MODAL:
             return { ...state, addStoryModal: action.payload }
         case types.SET_SELECTED_STORY:
-        console.log('dispatched')
+            console.log('dispatched')
             return { ...state, selectedStory: action.payload }
         default:
             throw new Error('should match a case');
