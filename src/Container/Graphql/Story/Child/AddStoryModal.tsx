5fdd32323320f2dccd890047963a3f0208d281ce
Prabeen04 <prabeen.strange@gmail.com>
2019-05-15 20:51:28 +0530

    form moved inside Mutaion componnet

--- src/Container/Graphql/Story/Child/AddStoryModal.tsx
+++ src/Container/Graphql/Story/Child/AddStoryModal.tsx
@@ -32,33 +32,31 @@ export default function AddStoryModal() {
             <>
                 <Mutation mutation={ADD_STORY}>
                     {
-                        (addStory, { data }) => {
-                            console.log(addStory)
-                            console.log(data)
-                            return <p>sdlkhsjh</p>
+                        (addStory, { loading, data, error }) => {
+                            return (
+                                <form onSubmit={handleSubmit}>
+                                    <TextInput
+                                        className='form-control'
+                                        name='title'
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setStoryTitle(e.target.value)}
+                                        value={storyTitle}
+                                        placeholder="What's the story ?"
+                                    /><br />
+                                    <SelectInput
+                                        className='form-control'
+                                        name='title'
+                                        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setUser(e.target.value)}
+                                        value={user}
+                                        placeholder="What's the story ?"
+                                        options={[{ label: 'one', value: 1 }, { label: 'two', value: 2 }, { label: 'three', value: 3 },]}
+                                    /><br />
+                                    <button className='btn btn-primary'>Add</button>
+                                </form>
+                            )
                         }
                     }
                 </Mutation>
-                <form onSubmit={handleSubmit}>
-                    <TextInput
-                        className='form-control'
-                        name='title'
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setStoryTitle(e.target.value)}
-                        value={storyTitle}
-                        placeholder="What's the story ?"
-                    /><br />
-                    <SelectInput
-                        className='form-control'
-                        name='title'
-                        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setUser(e.target.value)}
-                        value={user}
-                        placeholder="What's the story ?"
-                        options={[{ label: 'one', value: 1 }, { label: 'two', value: 2 }, { label: 'three', value: 3 },]}
-                    /><br />
-                    <button
-                        className='btn btn-primary'
-                    >Add</button>
-                </form>
+
             </>
         </Modal>
     )
