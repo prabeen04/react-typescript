b6b9091e429fa04b8ef3caf34d1bf55f62fce4c9
Prabeen04 <prabeen.strange@gmail.com>
2019-05-17 09:14:04 +0530

    ApolloProvider moved to top level App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { Switch, Route } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import Navbar from './Component/Navbar/Navbar';
 import Landing from './Component/Landing/Landing';
 import Todo from './Component/Todo/Todo';
@@ -9,6 +12,9 @@ import Movie from './Container/Movie/Movie';
 import Graphql from './Container/Graphql/Graphql';
 
 import './App.css';
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 interface IAppProps { }
 interface IAppState { }
@@ -18,19 +24,23 @@ export const Style = React.createContext('')
 class App extends React.Component<IAppProps, IAppState> {
   render() {
     return (
-      <Style.Provider value='none'>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/todo' component={Todo} />
-            <Route exact path='/counter' component={Counter} />
-            <Route exact path='/fetch' component={Fetch} />
-            <Route exact path='/Movie' component={Movie} />
-            <Route exact path='/graphql' component={Graphql} />
-          </Switch>
-        </div>
-      </Style.Provider>
+      <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+          <Style.Provider value='none'>
+            <div className="App">
+              <Navbar />
+              <Switch>
+                <Route exact path='/' component={Landing} />
+                <Route exact path='/todo' component={Todo} />
+                <Route exact path='/counter' component={Counter} />
+                <Route exact path='/fetch' component={Fetch} />
+                <Route exact path='/Movie' component={Movie} />
+                <Route exact path='/graphql' component={Graphql} />
+              </Switch>
+            </div>
+          </Style.Provider>
+        </ApolloHooksProvider>
+      </ApolloProvider>
     );
   }
 }
--- src/Container/Graphql/Graphql.tsx
+++ src/Container/Graphql/Graphql.tsx
@@ -1,28 +1,16 @@
 import * as React from 'react'
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
-
 import Story from './Story/Story'
 import './Graphql.css';
-
-const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql"
-});
 
 export default function Graphql(): JSX.Element {
     return (
         <div>
-            <ApolloProvider client={client}>
-                <ApolloHooksProvider client={client}>
-                    <>
-                        {window.location.origin.includes('localhost')
-                            ? <Story />
-                            : <h1 style={{ color: '#eee', textAlign: 'center', marginTop: 100 }}>Under construction :(</h1>
-                        }
-                    </>
-                </ApolloHooksProvider>
-            </ApolloProvider>
+            <>
+                {window.location.origin.includes('localhost')
+                    ? <Story />
+                    : <h1 style={{ color: '#eee', textAlign: 'center', marginTop: 100 }}>Under construction :(</h1>
+                }
+            </>
         </div>
     )
 }
