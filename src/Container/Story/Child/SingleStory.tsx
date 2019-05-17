5e1df10d17e998bf92054626e6aaaa77fec2527f
Prabeen04 <prabeen.strange@gmail.com>
2019-05-17 20:19:56 +0530

    router push, story detail

--- src/Container/Story/Child/SingleStory.tsx
+++ src/Container/Story/Child/SingleStory.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react';
+import { withRouter } from "react-router-dom";
+import { RouteComponentProps } from "react-router";
 import * as types from '../StoryActionTypes';
 import { IStory } from '../StoryInterface';
-import useStory from "../useStory";
 import StoryContext from '../StoryContext'
-interface ISingleStory {
+
+interface ISingleStory extends RouteComponentProps<any> {
     story: IStory;
+
 }
-export default function SingleStory(props: ISingleStory): JSX.Element {
+const SingleStory = (props: ISingleStory): JSX.Element => {
     const { state, dispatch } = React.useContext(StoryContext)
+    console.log(props)
+    function handleStoryDetailpush() {
+        dispatch({ type: types.SET_SELECTED_STORY, payload: props.story.id })
+        props.history.push({
+            pathname: `/story/${props.story.id}`,
+            state: { story: props.story }
+        })
+    }
     return (
         <h1
             data-test="SingleStory"
             className='story-title'
-            onClick={() => dispatch({ type: types.SET_SELECTED_STORY, payload: props.story.id })}
+            onClick={handleStoryDetailpush}
         >
             {props.story.title}
         </h1>)
 }
+export default withRouter(SingleStory)
