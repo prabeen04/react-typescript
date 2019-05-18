a21e41b32fdc0ba9f4f5579b08cee9246ed8554b
Prabeen04 <prabeen.strange@gmail.com>
2019-05-18 13:59:25 +0530

    Empty component creatd

---
+++ src/Component/Common/Empty/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { IEmpty } from "./interface";
+
+export default function Empty() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/Component/Common/Empty/interface.ts
@@ -0,0 +1,6 @@
+export interface IEmpty {
+    description: String;
+    children?: JSX.Element;
+    image?: HTMLImageElement | String;
+    rest?: any[]
+}
