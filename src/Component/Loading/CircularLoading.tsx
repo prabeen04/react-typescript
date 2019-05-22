6dacd778a556bbdc537a0f064bff2b14d7d2ddfa
Prabeen04 <prabeen.strange@gmail.com>
2019-05-22 21:10:45 +0530

    CirclarLoading component created

---
+++ src/Component/Loading/CircularLoading.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function CircularLoading() {
+    return (
+        <div className="flex-container center-align-row" style={{ width: '100%', padding: 100 }}>
+            <div className="loading"></div>
+        </div>
+    )
+}
