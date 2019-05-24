b6e9fe587e496776ea2df443212b5a3c47d354db
Prabeen04 <prabeen.strange@gmail.com>
2019-05-24 21:02:57 +0530

    useQuery used for Storydetail query

--- src/Container/Story/Child/StoryDetail/StoryDetail.tsx
+++ src/Container/Story/Child/StoryDetail/StoryDetail.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
 import { RouteComponentProps } from "react-router";
-import { Query } from "react-apollo";
+import { useQuery } from "react-apollo-hooks";
 import moment from 'moment';
 import StoryDetailHeader from './StoryDetailHeader';
 import CircularLoading from '../../../../Component/Loading/CircularLoading';
@@ -10,36 +10,31 @@ interface IStoryDetailProps extends RouteComponentProps<any> { }
 
 function StoryDetail(props: IStoryDetailProps) {
   const storyId = props.match.params.id; //get storyId from url
+  const { data, error, loading } = useQuery(GET_STORY_DETAIL, {
+    variables: { storyId },
+    skip: !storyId
+  })
+  if (loading) return <CircularLoading />;
+  if (error) return <p>Error :(</p>;
   return (
     <>
       <StoryDetailHeader />
       <div className='story-detail' data-test='StoryDetail'>
         {storyId &&
-          <Query query={GET_STORY_DETAIL}
-            variables={{ storyId }}
-            skip={!storyId}
-          >
-            {({ loading, error, data }: any) => {
-              console.log(data)
-              if (loading) return <CircularLoading />;
-              if (error) return <p>Error :(</p>;
-              return (
-                <>
-                  <div className='story-detail-heading'>
-                    <div>
-                      <h3>{data.getStoryById.title} </h3>
-                      <p className='author'>{data.getStoryById.user.userName}</p>
-                    </div>
-                  </div>
-                  <div className="article-body">
-                    <p className='inside-author'>{data.getStoryById.user.userName}</p>
-                    <p className='inside-author'>{moment(data.getStoryById.createdAt).format('lll')}</p>
-                    <div dangerouslySetInnerHTML={{ __html: data.getStoryById.article }} className='article-body' />
-                  </div>
-                </>
-              );
-            }}
-          </Query>}
+          <>
+            <div className='story-detail-heading'>
+              <div>
+                <h3>{data.getStoryById.title} </h3>
+                <p className='author'>{data.getStoryById.user.userName}</p>
+              </div>
+            </div>
+            <div className="article-body">
+              <p className='inside-author'>{data.getStoryById.user.userName}</p>
+              <p className='inside-author'>{moment(data.getStoryById.createdAt).format('lll')}</p>
+              <div dangerouslySetInnerHTML={{ __html: data.getStoryById.article }} className='article-body' />
+            </div>
+          </>
+        }
       </div>
     </>
   )
