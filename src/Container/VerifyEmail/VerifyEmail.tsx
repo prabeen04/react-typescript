d364fb15a8f5b29f0c59fafc38d86f4dd95e8dd9
Prabeen04 <prabeen.strange@gmail.com>
2019-06-13 21:57:35 +0530

    verifyEmail mutaion

--- src/Container/VerifyEmail/VerifyEmail.tsx
+++ src/Container/VerifyEmail/VerifyEmail.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { useMutation } from 'react-apollo-hooks';
+import gql from 'graphql-tag';
 
-interface IVerifyEmail extends RouteComponentProps { }
+interface IVerifyEmail extends RouteComponentProps<any> { }
 
 function VerifyEmail(props: IVerifyEmail) {
     const { match: { params } } = props;
     console.log(params)
+    const token: string = params.token
+    const VERIFY_EMAIL = gql`
+    mutation VerifyEmail($token: String!) {
+      verifyEmail( token: token )   
+       }
+  `;
+
+    // React.useEffect(() => {
+        const addStory = useMutation(VERIFY_EMAIL, { update: () => { } })
+        console.log(addStory)
+    // }, [token])
     return (
         <div>
             <h1>Verify your email</h1>
@@ -13,4 +26,4 @@ function VerifyEmail(props: IVerifyEmail) {
     )
 }
 
-export default withRouter(VerifyEmail)
+export default withRouter<IVerifyEmail>(VerifyEmail)
