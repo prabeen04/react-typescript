730ef88f1bfce0f64c2ab14481c2b2030f11b52b
Prabeen04 <prabeen.strange@gmail.com>
2019-06-21 20:33:48 +0530

    VerifyEmail test

---
+++ src/Container/Auth/VerifyEmail/VerifyEmail.test.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { shallow } from "enzyme";
+import VerifyEmail from './VerifyEmail';
+
+describe('<VerifyEmail/>', () => {
+    const props = {
+        // story: {
+        //     id: 'id1',
+        //     title: 'test'
+        // }
+    }
+    it('should match snapshot', () => {
+        const wrapper = shallow(<VerifyEmail {...props} />)
+        expect(wrapper).toMatchSnapshot()
+    })
+    it('should render VerifyEmail', () => {
+        const wrapper = shallow(<VerifyEmail {...props} />)
+
+        expect(wrapper.find('[data-test="VerifyEmail"]').length).toEqual(1)
+    })
+})
