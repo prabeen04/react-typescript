07ef84989d72c6682f21094bc6eef2e835029331
Prabeen04 <prabeen.strange@gmail.com>
2019-06-26 21:23:22 +0530

    empty movie case

--- src/Container/Movie/Child/MovieList.tsx
+++ src/Container/Movie/Child/MovieList.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useSpring, useSprings, useTransition, animated } from "react-spring";
 import { IMovie } from "../MovieInterface";
 import MovieCard from './MovieCard';
+import Empty from '../../../Component/Common/Error';
 
 interface IMovieListProps {
     movies: IMovie[];
@@ -9,14 +10,7 @@ interface IMovieListProps {
 }
 export default function MovieList(props: IMovieListProps): JSX.Element {
     const { movies, searchText } = props;
-    // const transitions = useTransition(movies, (item: any) => item.id, {
-    //     from: ({ xy, width, height }) => ({ xy, width, height, opacity: 0 }),
-    //     enter: ({ xy, width, height }) => ({ xy, width, height, opacity: 1 }),
-    //     update: ({ xy, width, height }) => ({ xy, width, height }),
-    //     leave: { height: 0, opacity: 0 },
-    //     config: { mass: 5, tension: 500, friction: 100 },
-    //     trail: 25
-    //   })
+    if (!movies.length) return <Empty description='No movies found' />
     return (
         <animated.div>
             <div className='movie-list' data-test='MovieList'>
