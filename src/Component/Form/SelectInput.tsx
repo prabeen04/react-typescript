1407157dd90a4667580e0c7c69ce85b768d8e77c
Prabeen04 <prabeen.strange@gmail.com>
2019-07-07 20:27:57 +0530

    linting to selectInput

--- src/Component/Form/SelectInput.tsx
+++ src/Component/Form/SelectInput.tsx
@@ -1,30 +1,43 @@
-import * as React from 'react'
+import * as React from "react";
 
 export interface ISelectInputInputProps {
-    value?: string | number;
-    name: string;
-    onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
-    options: any;
-    defaultValue?: any;
-    placeholder?: string;
-    className?: string;
-    style?: object
+  value?: string | number;
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  options: any;
+  defaultValue?: any;
+  placeholder?: string;
+  className?: string;
+  style?: object;
 }
-export default function SelectInputInput(props: ISelectInputInputProps) {
-    const { name, value, placeholder, onChange, options, defaultValue, ...rest } = props;
-    return (
-        <>
-            <select
-                data-test='SelectInputInput'
-                name={name}
-                value={value}
-                defaultValue={defaultValue}
-                placeholder={placeholder}
-                onChange={onChange}
-                {...rest}
-            >
-                {options && options.map((option: any, i: number) => <option key={i} value={option.value}>{option.label}</option>)}
-            </select>
-        </>
-    )
+export default function SelectInputInput(props: ISelectInputInputProps): any {
+  const {
+    name,
+    value,
+    placeholder,
+    onChange,
+    options,
+    defaultValue,
+    ...rest
+  } = props;
+  return (
+    <>
+      <select
+        data-test="SelectInputInput"
+        name={name}
+        value={value}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        onChange={onChange}
+        {...rest}
+      >
+        {options &&
+          options.map((option: any, i: number): any => (
+            <option key={i} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+      </select>
+    </>
+  );
 }
