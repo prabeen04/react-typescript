fa83c2ea2b8b2ffe94a5662b2600ae293a63dd62
Prabeen04 <prabeen.strange@gmail.com>
2019-07-09 19:35:38 +0530

    linting fix

--- src/Container/Story/Child/SingleStory.tsx
+++ src/Container/Story/Child/SingleStory.tsx
@@ -1,85 +1,99 @@
 import * as React from 'react';
-import { withRouter } from "react-router-dom";
-import { RouteComponentProps } from "react-router";
+import { withRouter } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router';
 import moment from 'moment';
-import { useMutation } from "react-apollo-hooks";
+import { useMutation } from 'react-apollo-hooks';
 import * as types from '../StoryActionTypes';
 import { IStory } from '../StoryInterface';
-import { DELETE_STORY, GET_STORIES } from "../StoryQuery";
-import StoryContext from '../StoryContext'
+import { DELETE_STORY, GET_STORIES } from '../StoryQuery';
+import StoryContext from '../StoryContext';
 
 interface ISingleStory extends RouteComponentProps<any> {
-    story: IStory;
-
+  story: IStory;
 }
 const SingleStory = (props: ISingleStory): JSX.Element => {
-    const { state, dispatch } = React.useContext(StoryContext)
-    function handleStoryDetailpush() {
-        dispatch({ type: types.SET_SELECTED_STORY, payload: props.story.id })
-        props.history.push({
-            pathname: `/story/${props.story.id}`,
-            state: { story: props.story }
-        })
-    }
-    function handleDeleteStory(e: React.SyntheticEvent, deleteStory: any, id: string) {
-        e.stopPropagation();
-        let allowDelete: any = confirm("Are you sure ?")
-        if (allowDelete) {
-            console.log('after confirm')
-            deleteStory({
-                variables: {
-                    storyId: id
-                }
-            })
+  const { state, dispatch } = React.useContext(StoryContext);
+  function handleStoryDetailpush() {
+    dispatch({ type: types.SET_SELECTED_STORY, payload: props.story.id });
+    props.history.push({
+      pathname: `/story/${props.story.id}`,
+      state: { story: props.story }
+    });
+  }
+  function handleDeleteStory(
+    e: React.SyntheticEvent,
+    deleteStory: any,
+    id: string
+  ) {
+    e.stopPropagation();
+    let allowDelete: any = confirm('Are you sure ?');
+    if (allowDelete) {
+      console.log('after confirm');
+      deleteStory({
+        variables: {
+          storyId: id
         }
+      });
     }
-    function handleUpdateStory(e: React.SyntheticEvent, story: IStory) {
-        e.stopPropagation();
-        dispatch({
-            type: types.SET_EDITING_STATE,
-            payload: {
-                isEditing: true,
-                addStoryModal: true,
-                currentEditingStory: story,
-            }
-        })
-    }
-    function updateStoriesAfterDelete(cache: any, { data: { deleteStory } }: any) {
-        const { stories } = cache.readQuery({ query: GET_STORIES });
-        console.log(stories, deleteStory)
-        const updatedStories = stories.filter((story: IStory) => story.id !== deleteStory.id)
-        cache.writeQuery({
-            query: GET_STORIES,
-            data: { stories: updatedStories },
-        });
-    }
-    const deleteStory = useMutation(DELETE_STORY, { variables: { storyId: props.story.id }, update: updateStoriesAfterDelete })
+  }
+  function handleUpdateStory(e: React.SyntheticEvent, story: IStory) {
+    e.stopPropagation();
+    dispatch({
+      type: types.SET_EDITING_STATE,
+      payload: {
+        isEditing: true,
+        addStoryModal: true,
+        currentEditingStory: story
+      }
+    });
+  }
+  function updateStoriesAfterDelete(
+    cache: any,
+    { data: { deleteStory } }: any
+  ) {
+    const { stories } = cache.readQuery({ query: GET_STORIES });
+    console.log(stories, deleteStory);
+    const updatedStories = stories.filter(
+      (story: IStory) => story.id !== deleteStory.id
+    );
+    cache.writeQuery({
+      query: GET_STORIES,
+      data: { stories: updatedStories }
+    });
+  }
+  const deleteStory = useMutation(DELETE_STORY, {
+    variables: { storyId: props.story.id },
+    update: updateStoriesAfterDelete
+  });
 
-    return (
-        <>
-            <div className="single-story"
-                onClick={handleStoryDetailpush}
-            >
-                <i className="fas fa-book-reader"></i>
-                <div style={{ flexBasis: '95%' }}>
-                    <h1
-                        data-test="SingleStory"
-                        className='story-title'
-                    >
-                        {props.story.title}
-                    </h1>
-                    <p className='story-time'>{moment(props.story.createdAt).format('ll')}</p>
-                </div>
-                <div className="story-action-wrapper">
-                    <i className="fas fa-edit"
-                        onClick={(e: React.SyntheticEvent) => handleUpdateStory(e, props.story)}
-                    ></i>
-                    <i className="fas fa-trash-alt"
-                        onClick={(e: React.SyntheticEvent) => handleDeleteStory(e, deleteStory, props.story.id)}
-                    ></i>
-                </div>
-            </div>
-        </>
-    )
-}
-export default withRouter(SingleStory)
+  return (
+    <>
+      <div className="single-story" onClick={handleStoryDetailpush}>
+        <i className="fas fa-book-reader"></i>
+        <div style={{ flexBasis: '95%' }}>
+          <h1 data-test="SingleStory" className="story-title">
+            {props.story.title}
+          </h1>
+          <p className="story-time">
+            {moment(props.story.createdAt).format('ll')}
+          </p>
+        </div>
+        <div className="story-action-wrapper">
+          <i
+            className="fas fa-edit"
+            onClick={(e: React.SyntheticEvent) =>
+              handleUpdateStory(e, props.story)
+            }
+          ></i>
+          <i
+            className="fas fa-trash-alt"
+            onClick={(e: React.SyntheticEvent) =>
+              handleDeleteStory(e, deleteStory, props.story.id)
+            }
+          ></i>
+        </div>
+      </div>
+    </>
+  );
+};
+export default withRouter(SingleStory);
--- src/Container/Story/Child/StoryList.tsx
+++ src/Container/Story/Child/StoryList.tsx
@@ -1,35 +1,37 @@
-import * as React from 'react'
-import { useQuery } from "react-apollo-hooks";
+import * as React from 'react';
+import { useQuery } from 'react-apollo-hooks';
 import moment from 'moment';
-import * as types from '../StoryActionTypes'
+import * as types from '../StoryActionTypes';
 import SingleStory from './SingleStory';
-import { IStory } from "../StoryInterface";
-import StoryContext from "../StoryContext";
-import Empty from "../../../Component/Common/Empty";
-import Error from "../../../Component/Common/Error";
+import { IStory } from '../StoryInterface';
+import StoryContext from '../StoryContext';
+import Empty from '../../../Component/Common/Empty';
+import Error from '../../../Component/Common/Error';
 import CircularLoading from '../../../Component/Loading/CircularLoading';
-import { GET_STORIES } from "../StoryQuery";
+import { GET_STORIES } from '../StoryQuery';
 
 const StoryList = (): JSX.Element => {
-    const { state, dispatch } = React.useContext(StoryContext)
-    const { data, loading, error } = useQuery(GET_STORIES)
-    if (loading) return <CircularLoading />;
-    if (error) return <Error description='Opps!!, some error occoured' />;
-    if (data.stories && data.stories.length) {
-        if (!state.selectedStory) {
-            dispatch({ type: types.SET_SELECTED_STORY, payload: data.stories[0].id })
-        }
+  const { state, dispatch } = React.useContext(StoryContext);
+  const { data, loading, error } = useQuery(GET_STORIES);
+  if (loading) return <CircularLoading />;
+  if (error) return <Error description="Opps!!, some error occoured" />;
+  if (data.stories && data.stories.length) {
+    if (!state.selectedStory) {
+      dispatch({ type: types.SET_SELECTED_STORY, payload: data.stories[0].id });
     }
-    return (
-        <div data-test="StoryList">
-            {
-                !data.stories && data.stories.length
-                    ? <Empty description='No Stories yet, Add your story' />
-                    : data.stories
-                        .sort((a: IStory, b: IStory) => moment(a.createdAt) < moment(b.createdAt) ? 1 : -1)
-                        .map((story: IStory, i: any) => <SingleStory key={i} story={story} />)
-            }
-        </div>
-    )
+  }
+  return (
+    <div data-test="StoryList">
+      {!data.stories && data.stories.length ? (
+        <Empty description="No Stories yet, Add your story" />
+      ) : (
+        data.stories
+          .sort((a: IStory, b: IStory) =>
+            moment(a.createdAt) < moment(b.createdAt) ? 1 : -1
+          )
+          .map((story: IStory, i: any) => <SingleStory key={i} story={story} />)
+      )}
+    </div>
+  );
 };
 export default StoryList;
